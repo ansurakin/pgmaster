ce62c59c7fd8a0b3f87a96ede27c3e6dc9566d2d
alex <alex@example.com>
2020-12-12 14:39:23 +0300

    first commit

---
+++ src/test/java/com/alex/pgmaster/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.pgmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
